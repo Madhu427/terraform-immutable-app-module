891967a3bf49128cdabf04fc34b28fff10c80d87
Madhu427 <madhusudhanachary.k@gmail.com>
2022-05-01 13:52:24 +0530

    }

--- asg.tf
+++ asg.tf
@@ -7,16 +7,12 @@ resource "aws_autoscaling_group" "asg" {
     id      = aws_launch_template.launch-template.id
     version = "$Latest"
   }
-}
+
   vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
-
-
 
   tag {
     key                 = "Name"
     value               = "${var.COMPONENT}-${var.APP_VERSION}"
     propagate_at_launch = true
   }
-
-
 }
