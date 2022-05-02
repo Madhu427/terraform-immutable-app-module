234bc9c7b6acb1f20dfa02a5ea85159f8e57e0d2
Madhu427 <madhusudhanachary.k@gmail.com>
2022-05-02 15:17:04 +0530

    "${var.COMPONENT}-${var.ENV}" --> corrected

--- launch-template.tf
+++ launch-template.tf
@@ -2,8 +2,6 @@ resource "aws_launch_template" "launch-template" {
   name = "${var.COMPONENT}-${var.ENV}"
   image_id = "data.aws_ami.ami.id"
 
-
-
   instance_market_options {
     market_type = "spot"
     spot_options {
@@ -13,17 +11,13 @@ resource "aws_launch_template" "launch-template" {
   }
 
   instance_type = "var.INSTANCE_TYPE"
-
-
-
-
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "${var.COMPONENT}-${var.APP_VERSION}"
+      Name = "${var.COMPONENT}-${var.ENV}"
     }
   }
 
