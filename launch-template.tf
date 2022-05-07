db5a54ffa58e2473ba4094658ea4df61b4e7b8df
Madhu427 <madhusudhanachary.k@gmail.com>
2022-05-07 18:10:32 +0530

    spot_instance_type disabled

--- launch-template.tf
+++ launch-template.tf
@@ -5,9 +5,9 @@ resource "aws_launch_template" "launch-template" {
   instance_market_options {
     market_type = "spot"
     spot_options {
-#      instance_interruption_behavior = "stop"
+      instance_interruption_behavior = "stop"
 #      spot_instance_type = "persistent"
-      spot_instance_type = "one-time"
+#      spot_instance_type = "one-time"
 
     }
   }
